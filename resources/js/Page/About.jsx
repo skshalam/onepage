bd40a622ce9d7de534a95e5850120a18f3fbef67
Akash-ewards-2024 <159058367+ash50167@users.noreply.github.com>
2024-09-18 18:09:41 +0530

    thiss

--- resources/js/Page/About.jsx
+++ resources/js/Page/About.jsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 function About() {
     const [data_getcoupons, setData_getcoupons] = useState({
         active_coupon_count: 0,
@@ -83,8 +85,8 @@ function About() {
                         <div className={`headlogo-img ${data_home.display_brand_logo_name === 1 ? 'ratio-3' : 'ratio-1'}`}>
                             {
                                 loading
-                                ? <div className="cust-skeleton-loader header-img"></div>
-                                :<img src={data_home.homebrandlogo} alt="" />
+                                    ? <div className="cust-skeleton-loader header-img"></div>
+                                    : <img src={data_home.homebrandlogo} alt="" />
                             }
                         </div>
                         {data_home.display_brand_logo_name === 2 && (
@@ -95,60 +97,66 @@ function About() {
                     </div>
                     <div className="head-profile">
                         <Link className='text-decoration-none' to={"/Profile"}>
-                        <i className='bi bi-person-fill fs-5'></i>
+                            <i className='bi bi-person-fill fs-5'></i>
                         </Link>
                     </div>
                 </div>
-                {data_home.banners?.banner_image?.length > 0 
-                
-                ? (
-                    <div className='banner-part-main'>
-                        <div className='banner-part'>
-                            <Slider {...settings}>
-                                {data_home.banners.banner_image.map((banner, index) => (
-                                    <div key={index}>
-                                        <img src={banner} alt={`Banner ${index + 1}`} />
-                                    </div>
-                                ))}
-                            </Slider>
+                {data_home.banners?.banner_image?.length > 0
+
+                    ? (
+                        <div className='banner-part-main'>
+                            <div className='banner-part'>
+                                <Slider {...settings}>
+                                    {data_home.banners.banner_image.map((banner, index) => (
+                                        <div key={index}>
+                                            <img src={banner} alt={`Banner ${index + 1}`} />
+                                        </div>
+                                    ))}
+                                </Slider>
+                            </div>
                         </div>
-                    </div>
-                )
-                :<div className="cust-skeleton-loader banner-skl"></div>
-            }
+                    )
+                    : <div className="cust-skeleton-loader banner-skl"></div>
+                }
                 <div className={`wallet-container gap-2 m-3 ${data_home.banners?.banner_image?.length > 0 ? '' : 'wallet-banner'}`}>
                     <Link className='text-decoration-none' to={"/Creditwallet"}>
-                    <div className="wallet-content">
-                        <div className="content">
-                            <p>Credit Balance:</p>
-                            <div className="balance">
-                                <img src="" alt="" />
-                                <span>
-                                    {
-                                        loading?<div className="cust-skeleton-loader header-img"></div>
-                                    :data_home.cards.current_points
-                                    }
-                                </span>
+                        <div className="wallet-content">
+                            <div className="content">
+                                <p>Credit Balance:</p>
+                                {loading ?
+                                    <SkeletonTheme baseColor="#c7c7c7" highlightColor="#ffffff">
+                                        <Skeleton />
+                                    </SkeletonTheme>
+                                    : <div className="balance">
+                                        <img src="" alt="" />
+                                        <span>
+                                            {data_home.cards.current_points}
+                                        </span>
+                                    </div>}
+                            </div>
+                            <div className="content">
+                                <i className="bi bi-chevron-right"></i>
                             </div>
                         </div>
-                        <div className="content">
-                            <i className="bi bi-chevron-right"></i>
-                        </div>
-                    </div>
                     </Link>
                     <Link className='text-decoration-none' to={"/Wallet"}>
-                    <div className="wallet-content">
-                        <div className="content">
-                            <p>Wallet's Balance:</p>
-                            <div className="balance">
-                                <img src="" alt="" />
-                                <span>{data_home.cards.current_wallet_balance}</span>
+                        <div className="wallet-content">
+                            <div className="content">
+                                <p>Wallet's Balance:</p>
+                                {
+                                    loading ?
+                                        <SkeletonTheme baseColor="#c7c7c7" highlightColor="#ffffff">
+                                            <Skeleton />
+                                        </SkeletonTheme>
+                                        : <div className="balance">
+                                            <img src="" alt="" />
+                                            <span>{data_home.cards.current_wallet_balance}</span>
+                                        </div>}
+                            </div>
+                            <div className="content">
+                                <i className="bi bi-chevron-right"></i>
                             </div>
                         </div>
-                        <div className="content">
-                            <i className="bi bi-chevron-right"></i>
-                        </div>
-                    </div>
                     </Link>
                 </div>
                 <div className="rewards-container gap-2 m-3">
@@ -156,10 +164,16 @@ function About() {
                         <div className="rewards-content">
                             <div className="content">
                                 <p>Coupon <br /> Cart</p>
-                                <div className="balance">
-                                    <img src="" alt="" />
-                                    <span>{data_getcoupons.active_coupon_count}</span>
-                                </div>
+                                {
+                                    loading ?
+                                        <SkeletonTheme baseColor="#c7c7c7" highlightColor="#ffffff">
+                                            <Skeleton />
+                                        </SkeletonTheme>
+                                        : <div className="balance">
+                                            <img src="" alt="" />
+                                            <span>{data_getcoupons.active_coupon_count}</span>
+                                        </div>
+                                }
                             </div>
                             <div className="content">
                                 <i className="bi bi-chevron-right"></i>
@@ -170,10 +184,15 @@ function About() {
                         <div className="rewards-content">
                             <div className="content">
                                 <p>Reward <br /> Menu</p>
-                                <div className="balance">
+                                {loading ?
+                                        <SkeletonTheme baseColor="#c7c7c7" highlightColor="#ffffff">
+                                            <Skeleton />
+                                        </SkeletonTheme>
+                                        :
+                                    <div className="balance">
                                     <img src="" alt="" />
                                     <span>{data_getcoupons.active_rewards_count}</span>
-                                </div>
+                                </div>}
                             </div>
                             <div className="content">
                                 <i className="bi bi-chevron-right"></i>
@@ -184,10 +203,15 @@ function About() {
                         <div className="rewards-content">
                             <div className="content">
                                 <p>Membership Package</p>
-                                <div className="balance">
+                                {loading ?
+                                        <SkeletonTheme baseColor="#c7c7c7" highlightColor="#ffffff">
+                                            <Skeleton />
+                                        </SkeletonTheme>
+                                        :
+                                    <div className="balance">
                                     <img src="" alt="" />
                                     <span>{data_getcoupons.active_membership_count}</span>
-                                </div>
+                                </div>}
                             </div>
                             <div className="content">
                                 <i className="bi bi-chevron-right"></i>
