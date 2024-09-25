ef65be444622cf0092dbd3fb2ebc108ac056bab6
arnab <arnab.myewards@gmail.com>
2024-09-25 20:27:53 +0530

    this

---
+++ app/Models/MerchantLoginMenuItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MerchantLoginMenuItem extends Model
+{
+    use HasFactory;
+    protected $table = 'merchant_login_menu_items';
+}
