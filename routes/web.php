9ff07bff6244e68998edb7c0beb43b70ca00e648
Saha alam <sksahaalam3@gmail.com>
2025-09-14 19:57:04 +0530

    this

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 // Route::get('/onePageWebsite/{m_id}',[\App\Http\Controllers\HomeController::class, 'onePageWebsite']);
 
+// Route::get('/onePageWebsite/{m_id}',[\App\Http\Controllers\HomeController::class, 'onePageWebsite']);
+
 Route::get('/{any}', function () {
     return view('welcome'); // This should be the Blade view that includes your React app
 })->where('any', '.*');
